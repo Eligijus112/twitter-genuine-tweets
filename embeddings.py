63645dc5753950863a009da318eb358274bebd0e
Eligijus Bujokas <elasorama@gmail.com>
2020-03-09 21:02:14 +0200

    abstracted the code a bit

--- embeddings.py
+++ embeddings.py
@@ -18,14 +18,21 @@ class Embeddings():
         embeddings_index = dict(self.get_coefs(*o.split(" ")) for o in open(self.path, errors='ignore'))
         return embeddings_index
 
-    def create_embedding_matrix(self, tokenizer, max_features):
+    def create_embedding_matrix(self, tokenizer=None, max_features=None):
         """
         A method to create the embedding matrix
         """
         model_embed = self.get_embedding_index()
 
+        if max_features is None:
+            max_features = len(model_embed)
+
+        word_index = model_embed
+        if tokenizer is None: 
+            word_index = tokenizer.word_index
+
         embedding_matrix = np.zeros((max_features + 1, self.vector_dimension))
-        for word, index in tokenizer.word_index.items():
+        for word, index in word_index.items():
             if index > max_features:
                 break
             else:
